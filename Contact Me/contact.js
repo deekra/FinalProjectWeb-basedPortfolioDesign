8f917087b0f5c2889475e1db7ab87115e08fc991
deekra <114116356+deekra@users.noreply.github.com>
2022-11-30 10:22:24 -0800

    Update contact.js

--- Contact Me/contact.js
+++ Contact Me/contact.js
@@ -1,1 +1,12 @@
 
+var name = Name ; 
+var subject = Subject; 
+var email = Email
+var additional = Comment;                                             
+var msg = 'Thank you!';   // Message
+
+<form action="Contact Me/index.html" method="get"
+name="myContact">
+
+</form>
+el.innerHTML = msg;
