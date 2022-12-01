544ace6bf8cf4b1c1e8530323dec2c3db10f6935
deekra <114116356+deekra@users.noreply.github.com>
2022-11-30 18:49:38 -0800

    Update contact.js

--- Contact Me/contact.js
+++ Contact Me/contact.js
@@ -3,14 +3,14 @@ var name = false;
 var email = false;
 var comment = false;
 var errorMessages ="";
-
+alert( "dummy" );
 /*  function validateName()
 { */
   var formname = document.getElementById("name").value;
   if (formname.value==="null"|| 
      formname.value==="" )
   {
-    alert( "The First Name is required, only letters are accepted and it cannot be greater than 20 characters." )
+    alert( "The First Name is required, only letters are accepted and it cannot be greater than 20 characters." );
     errorMessages += "<p>The Name is required.</p>";
   }
   else
