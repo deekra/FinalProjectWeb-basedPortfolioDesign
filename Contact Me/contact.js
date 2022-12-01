4ba53102d67f2eb8a0bc0f1590bdd9a037d3731f
deekra <114116356+deekra@users.noreply.github.com>
2022-11-30 18:07:40 -0800

    Update contact.js

--- Contact Me/contact.js
+++ Contact Me/contact.js
@@ -3,3 +3,20 @@ var name = false;
 var email = false;
 var comment = false;
 var errorMessages ="";
+
+  function validateName()
+{
+  var name = document.getElementById("name").value;
+  if (name.value==="null"|| 
+      name.value==="" )
+  {
+    errorMessages += "<p>The Name is required.</p>";
+  }
+  else
+  {
+    name = true;
+  } 
+  return (validname);
+}
+  return (validateName(name));
+}
